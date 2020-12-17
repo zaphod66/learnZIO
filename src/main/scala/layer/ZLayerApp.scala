2359e67c814b64d1a2162835e130624772809f0c
Norbert Scheller <norbert@riskident.com>
2020-12-17 13:33:18 +0100

    FuncScala2020 - yet another ZIO hello world

--- src/main/scala/layer/ZLayerApp.scala
+++ src/main/scala/layer/ZLayerApp.scala
@@ -1,9 +1,25 @@
 package layer
 
-import zio.{ExitCode, URIO, ZIO}
-import zio.console._
-import zio.ExitCode._
+import zio._
+import zio.console.Console
 
 object ZLayerApp extends zio.App {
-  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] = ???
+
+  type MeaningOfLive = Has[MeaningOfLive.Service]
+
+  val meaningOfLive = ZIO.environment[Has[Int] with Console].flatMap(env => console.putStrLn(s"The meaning of Live is ${env.get}."))
+  object MeaningOfLive {
+    trait Service
+  }
+
+  val consoleLayer = zio.console.Console.live
+  val intLayer = ZLayer.fromManaged {
+    ZManaged.make(UIO(println("Acquiring Int")).as(42)){ _ => UIO.effectTotal(println("Releasing Int"))}
+  }
+
+  val combinedLayer = intLayer ++ consoleLayer
+
+
+  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] =
+    meaningOfLive.provideLayer(combinedLayer).exitCode
 }
