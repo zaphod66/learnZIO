b89b6586b55633e23318d42347751663fb4c6925
Norbert Scheller <norbert@riskident.com>
2021-02-01 18:44:26 +0100

    resTest - real use of ZRef

--- src/main/scala/streams/ZStreamObfuscate.scala
+++ src/main/scala/streams/ZStreamObfuscate.scala
@@ -1,10 +1,11 @@
 package streams
 
 import zio._
+import zio.blocking.Blocking
 import zio.console.putStrLn
 import zio.stream._
 
-import java.io.{BufferedWriter, FileWriter}
+import java.io.{BufferedWriter, FileWriter, Writer}
 import java.nio.file.Paths
 import java.time.Instant
 
@@ -44,21 +45,6 @@ object ZStreamObfuscate extends App {
     m.get(k).fold(m + (k -> obf(k)))(_ => m)
   }
 
-  def mod2(m: Map[String, String], k: String, obf: String => String): (String, Map[String, String]) = {
-    m.get(k).fold {
-      val v = obf(k)
-      (v, m + (k -> v))
-    }(v => (v, m))
-  }
-
-  def obfuscateNVR(s: String, f: String => String, urm: UIO[Ref[Map[String, String]]]) = {
-    val fff = for {
-      rm <- urm
-      nv <- rm.modify(m => mod2(m, s, f))
-    } yield (nv, urm)
-    fff
-  }
-
   def obfuscateNV(s: String, f: String => String, m: mutable.Map[String, String]): String = {
     m.get(s).fold {
       val o = f(s)
@@ -87,64 +73,77 @@ object ZStreamObfuscate extends App {
     s"$na;${sa(1)};${sa(2)}"
   }
 
-  def obfuscateUE(in: UnconstraintExact): UnconstraintExact = {
-    val nvNew = in.mt match {
-      case "SameCustomerNumberMatcher$"  => obfuscateCustNo(in.nv)
-      case "SamePhoneNumberMatcher$"     => obfuscatePhone(in.nv)
-      case "SameShipmentAddressMatcher$" => obfuscateAddr(in.nv)
-      case "SameBillingAddressMatcher$"  => obfuscateAddr(in.nv)
-      case "SameEmailMatcher$"           => obfuscateEmail(in.nv)
-      case "SameDIExactIdMatcher$"       => obfuscateExact(in.nv)
-      case "SameDISmartIdMatcher$"       => obfuscateSmart(in.nv)
-    }
-
-    in.copy(nv = nvNew)
-  }
-
   trait Persistence {
     def update(f: Map[String, String] => Map[String, String]): ZIO[Any, Nothing, Unit]
+    def modify(f: Map[String, String] => (String, Map[String, String])): ZIO[Any, Nothing, String]
     def get: ZIO[Any, Nothing, Map[String, String]]
   }
 
   case class MapPersistence(rm: Ref[Map[String, String]]) extends Persistence {
     override def update(f: Map[String, String] => Map[String, String]): ZIO[Any, Nothing, Unit] = rm.update(f)
+    override def modify(f: Map[String, String] => (String, Map[String, String])): ZIO[Any, Nothing, String] = rm.modify(f)
     override def get: ZIO[Any, Nothing, Map[String, String]] = rm.get
   }
-
-  val mapZIO: ZIO[Any, Nothing, MapPersistence] = ZRef.make(Map.empty[String, String]).map(MapPersistence)
-  val mapLayer: ZLayer[Any, Nothing, Has[Persistence]] = mapZIO.toLayer
 
   trait Obfuscator {
     def tObfuscateUE(ue: UnconstraintExact): ZIO[Any, Nothing, UnconstraintExact]
   }
 
   case class PersObfuscator(pers: Persistence) extends Obfuscator {
-    override def tObfuscateUE(ue: UnconstraintExact): ZIO[Any, Nothing, UnconstraintExact] = UIO(ue.copy(nv = "v11"))
+    def modifyMap(s: String, e: String => String, f: String => String, m: Map[String, String]): (String, Map[String, String]) = {
+      val ext = e(s)
+      m.get(ext).fold {
+        val v = f(s)
+        (v, m + (ext -> v))
+      } (v => (v, m))
+    }
+
+    def createNumericString(s: String): String = numericString(s.length)
+    def createAddr(a: String): String = {
+      val sa = a.split(';')
+      val na = alphanumericString(sa(0).length)
+      s"$na;${sa(1)};${sa(2)}"
+    }
+    def createEmail(e: String): String = {
+      val se = e.split('@')
+      val na = alphanumericString(se(0).length)
+      s"$na@${se(1)}"
+    }
+    def createAlphanumericString(s: String): String = alphanumericString(s.length)
+
+    def modifyRef(ue: UnconstraintExact)(m: Map[String, String]): (String, Map[String, String]) = {
+      ue.mt match {
+        case "SameCustomerNumberMatcher$"  => modifyMap(ue.nv, identity, createNumericString, m)
+        case "SamePhoneNumberMatcher$"     => modifyMap(ue.nv, identity, createNumericString, m)
+        case "SameShipmentAddressMatcher$" => modifyMap(ue.nv, s => s.split(';')(0), createAddr  , m)
+        case "SameBillingAddressMatcher$"  => modifyMap(ue.nv, s => s.split(';')(0), createAddr  , m)
+        case "SameEmailMatcher$"           => modifyMap(ue.nv, s => s.split('@')(0), createEmail , m)
+        case "SameDIExactIdMatcher$"       => modifyMap(ue.nv, identity, createAlphanumericString, m)
+        case "SameDISmartIdMatcher$"       => modifyMap(ue.nv, identity, createAlphanumericString, m)
+      }
+    }
+
+    override def tObfuscateUE(ue: UnconstraintExact): ZIO[Any, Nothing, UnconstraintExact] = {
+      pers.modify(modifyRef(ue)).map(s => ue.copy(nv = s))
+    }
+
+    def mutableObfuscateUE(ue: UnconstraintExact): ZIO[Any, Nothing, UnconstraintExact] = {
+      val obfNv = ue.mt match {
+        case "SameCustomerNumberMatcher$"  => obfuscateCustNo(ue.nv)
+        case "SamePhoneNumberMatcher$"     => obfuscatePhone(ue.nv)
+        case "SameShipmentAddressMatcher$" => obfuscateAddr(ue.nv)
+        case "SameBillingAddressMatcher$"  => obfuscateAddr(ue.nv)
+        case "SameEmailMatcher$"           => obfuscateEmail(ue.nv)
+        case "SameDIExactIdMatcher$"       => obfuscateExact(ue.nv)
+        case "SameDISmartIdMatcher$"       => obfuscateSmart(ue.nv)
+      }
+      UIO(ue.copy(nv = obfNv))
+    }
   }
 
+  val mapZIO: ZIO[Any, Nothing, MapPersistence] = ZRef.make(Map.empty[String, String]).map(MapPersistence)
   val perZIO: ZIO[Any, Nothing, Obfuscator] = mapZIO.map(PersObfuscator)
   val perLayer:ZLayer[Any, Nothing, Has[Obfuscator]] = perZIO.toLayer
-
-  val ralMap = ZRef.make(Map.empty[String, String])
-  val zalMap = ralMap.toLayer
-
-  val zalLay = for {
-//    pers <- ZIO.service[Persistence]
-    obfu <- ZIO.service[Obfuscator]
-//    _  <- pers.update(_ + ("k11" -> "v11"))
-//    _  <- pers.update(_ + ("k12" -> "v12"))
-    u1 <- obfu.tObfuscateUE(UnconstraintExact("mt", "k11", "tid1", Instant.now()))
-    u2 <- obfu.tObfuscateUE(UnconstraintExact("mt", "k11", "tid2", Instant.now()))
-//    s  <- pers.get
-//    v  = s("k12")
-  } yield (u1, u2)
-
-  val zalPro = for {
-    per <- zalLay
-//    str <- per._2.get.map(_.toString)
-//    _   <- putStrLn(per._1 + ", " + str)
-    _ <- putStrLn(s"${per._1} = ${per._2}")
-  } yield ()
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
   //  val srcFilePath = "/Users/nschelle/work/gitrepos/zaphod66/learnZIO/unconstraintExact_0000010.csv"
@@ -156,46 +155,22 @@ object ZStreamObfuscate extends App {
     val fileStream  = ZStream.fromFile(Paths.get(srcFilePath))
       .transduce(ZTransducer.utf8Decode >>> ZTransducer.splitLines)
 
-    val stream = ZStream.managed(managedFile)
-      .flatMap{ bw =>
-        fileStream
-          .map(UnconstraintExact.make)
-          .map(obfuscateUE)
-          .tap(ue => ZIO(bw.write(s"${ue.toString}\n")))
-      }.runCount
-
-    val obfPr1 = for {
-      per <- ZIO.service[Obfuscator]
-      no  <- ZStream.managed(managedFile).flatMap { bw =>
-        fileStream
+    val obfPro = managedFile.use { bw =>
+      for {
+        per <- ZIO.service[Obfuscator]
+        no  <- fileStream
           .map(UnconstraintExact.make)
           .mapM(per.tObfuscateUE)
           .tap(ue => ZIO(bw.write(s"${ue.toString}\n")))
-      }.runCount
-    } yield no
-
-    val obfPr2 = for {
-      per <- ZIO.service[Obfuscator]
-      no  <- (for {
-                bw <- ZStream.managed(managedFile)
-              } yield fileStream
-                      .map(UnconstraintExact.make)
-                      .mapM(per.tObfuscateUE)
-                      .tap(ue => ZIO(bw.write(s"${ue.toString}\n")))
-             ).runCount
-    } yield no
+          .runCount
+      } yield no
+    }
 
     import zio.console.Console
     import zio.blocking.Blocking
-    val obfEnv = Console.live ++ perLayer ++ Blocking.live
-    val obfCo1 = obfPr1.provideLayer(obfEnv)
-    val obfCo2 = obfPr2.provideLayer(obfEnv)
+    val obfEnv  = Console.live ++ Blocking.live ++ perLayer
+    val program = obfPro.provideLayer(obfEnv)
 
-//    val zalEnv = Console.live ++ perLayer // ++ mapLayer
-//    val zalCom = zalPro.provideLayer(zalEnv)
-//    val program = stream.tap(c => putStrLn(s"processed $c lines.")) *>
-//      putStrLn("alMap: " + alMap.size.toString) *> zalCom *> obfHoo *> obfHo2
-    val program = obfCo2
     program.tap(l => putStrLn(s"Ref processed $l lines.")).exitCode
   }
 }
